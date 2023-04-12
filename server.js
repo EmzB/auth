3b0aa7d4b765ebff395980af2e7c46a9812e05a4
Bella <emalibella45@gmail.com>
2023-04-12 23:06:03 +0300

    connected

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ require('dotenv').config();
 
 //const userRoutes= require('./routes/user')
 console.log('MONGO_URI:', process.env.MONGO_URI);
-console.log('MONGO_URI:', process.env.PORT);
+console.log('PORT:', process.env.PORT);
 
 
 
 mongoose.connect(process.env.MONGO_URI).then(()=> {
  app.listen(process.env.PORT, () =>{
  console.log("CONNECTED")})
-    });
+    }).catch((error)=>{
+        console.log(error)
+    })
