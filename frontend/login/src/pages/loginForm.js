6450cc92c4216e5e439113a5c2d8b6a2f33f7fc5
Bella <emalibella45@gmail.com>
2023-04-25 23:21:28 +0300

    creating form

--- frontend/login/src/pages/loginForm.js
+++ frontend/login/src/pages/loginForm.js
@@ -1,8 +1,28 @@
+import {useSate} from 'react';
 
 const loginForm= () => {
+  const [email,  setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
 
 return (
-    <h1>Hi I'm a form</h1> 
+    <form>
+    <label>Email:</label>
+    <input
+    type="email"
+    onChange={(e)=> setEmail(e.target.value) }
+    value = {email}
+    />
+
+<label>Password:</label>
+    <input
+    type="password"
+    onChange={(e)=> setPassword(e.target.value) }
+    value = {password}
+    />
+
+
+    </form>
 )}
 
 export default loginForm;
