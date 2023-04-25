dd7c48593b7a2edad86467118f67a39b83fed5b9
Bella <emalibella45@gmail.com>
2023-04-25 23:01:16 +0300

    wrapping context

--- frontend/login/src/index.js
+++ frontend/login/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { authContextProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <authContextProvider>
     <App />
+    </authContextProvider>
+    
   </React.StrictMode>
 );
 
